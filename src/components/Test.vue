3151ee0f58d81d6a3e78565013587fcf7a477bd0
myCedar <1301172947@qq.com>
2018-07-23 13:17:45 +0800

    Initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="title">
+      <h3>sass test</h3>
+      <ul class="content">
+        <li>测试1</li>
+        <li>测试2</li>
+        <li>测试3</li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "Test"
+    }
+</script>
+
+<style scoped lang="scss">
+  .title{
+    color: red;
+    .content{
+      margin-top: 20px;
+      li{
+        font-size: 16px;
+        color: blue;
+      }
+    }
+  }
+</style>
